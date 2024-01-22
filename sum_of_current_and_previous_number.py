2db192b179bd44f38a5645c040516ec106b239ad
maihannn <maicapalillo15@gmail.com>
2024-01-22 12:27:51 +0800

    done, it worked

---
+++ sum_of_current_and_previous_number.py
@@ -0,0 +1,23 @@
+# Name: Jamaica C. Palillo
+# Section: BSCPE 1-5
+
+# Exercise 2: Print the sum of the current and previous numbers.
+
+# Getting the 10 numbers.
+
+previous_number = 0
+
+for i in range (0, 10):
+    # In each input, get the sum of the current and previous numbers.
+    sum = previous_number + i
+
+    print ("Current Number + Previous Number: ", i, " + ", previous_number, " = ", sum)
+    previous_number = i
+
+
+
+
+
+
+
+
